d02ff7585e05b67eba71e3ceb1ce4509cf82da46
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2021-11-01 08:48:56 +0100

    Added heading for footers

--- js/components/footer.js
+++ js/components/footer.js
@@ -10,6 +10,7 @@ class Footer extends HTMLElement {
 				<div class="row justify-content-around">
 					<div class="col-12 col-md-3"><img src="./img/logo.png" alt="" /></div>
 					<div class="col-12 col-md-5">
+					<h4>About us</h4>
 						<p>
 							Concinnity is a trusted partner in enabling organisations to
 							develop and realise their vision for success. We provide the
@@ -18,6 +19,7 @@ class Footer extends HTMLElement {
 						</p>
 					</div>
 					<div class="col-12 col-md-3">
+						<h4>Contact</h4>
 						<p>T: +234 7011 5914 38</p>
 						<p>E: hello@concinnity.com</p>
 					</div>
